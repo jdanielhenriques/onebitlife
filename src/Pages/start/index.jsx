fa516503090706a477b4a5c7f6141a0111d25e81
José Daniel Henriques Porto <jdanielhenriques@gmail.com>
2022-12-09 21:15:10 +0000

    Add default button with styles

--- src/Pages/start/index.jsx
+++ src/Pages/start/index.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import { View, ScrollView, Text, Image, StyleSheet } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import DefaultButton from "../../Components/Common/DefaultButton";
 import LifeStatus from "../../Components/Common/LifeStatus";
 
 export default function Start() {
+  const handleNavAppExplanation = () => {
+    console.log("Testando o click");
+  };
+
   return (
     <View style={styles.container}>
       <ScrollView showVerticalScrollIndicator={false}>
@@ -16,6 +22,12 @@ export default function Start() {
             Vamos transformar sua vida{"\n"}em jogo, buscando sempre{"\n"}o
             melhor nível.
           </Text>
+          <DefaultButton
+            buttonText={"Continuar"}
+            handlePress={handleNavAppExplanation}
+            width={250}
+            height={50}
+          />
         </View>
       </ScrollView>
     </View>
