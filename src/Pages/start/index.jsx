a90fdb859a2bcb54de59a1b177c45bbb93734c02
José Daniel Henriques Porto <jdanielhenriques@gmail.com>
2022-12-09 20:49:35 +0000

    Add lifestatus animation and components structure

--- src/Pages/start/index.jsx
+++ src/Pages/start/index.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { View, ScrollView, Text, Image, StyleSheet } from "react-native";
+import LifeStatus from "../../Components/Common/LifeStatus";
 
 export default function Start() {
   return (
     <View style={styles.container}>
       <ScrollView showVerticalScrollIndicator={false}>
-        <View style={{alignItems: "center"}}>
-          <Image source={require("../../assets/icons/logo3.png")}
-          style={styles.logo} />
+        <View style={{ alignItems: "center" }}>
+          <Image
+            source={require("../../assets/icons/logo3.png")}
+            style={styles.logo}
+          />
+          <LifeStatus />
           <Text style={styles.description}>
             Vamos transformar sua vida{"\n"}em jogo, buscando sempre{"\n"}o
             melhor nível.
@@ -29,9 +33,10 @@ const styles = StyleSheet.create({
     marginTop: 60,
     marginBottom: 20,
   },
-  description:{
-  color: "#ffffff",
-  fontSize: 20,
-  textAlign: "center",
-  marginVertical: 60},
+  description: {
+    color: "#ffffff",
+    fontSize: 20,
+    textAlign: "center",
+    marginVertical: 60,
+  },
 });
