5060378c1b44ec4dc9163e8e270a3c5861b50bf5
José Daniel Henriques Porto <jdanielhenriques@gmail.com>
2022-12-10 17:53:20 +0000

    Structure and routes from HomePage

--- src/Routes/HomePage.jsx
+++ src/Routes/HomePage.jsx
@@ -1,15 +1,12 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { Header } from "@react-navigation/stack";
-
-import Start from "../Pages/start";
 import AppExplanation from "../Pages/AppExplanation";
 import Home from "../Pages/Home";
 import HabitPage from "../Pages/HabitPage";
 
 const Stack = createNativeStackNavigator();
 
-export default function AllPages() {
+export default function HomePage() {
   return (
     <NavigationContainer>
       <Stack.Navigator
@@ -18,23 +15,16 @@ export default function AllPages() {
         }}
       >
         <Stack.Screen
-          name='Start'
-          component={Start}
-        />
-
-        <Stack.Screen
-          name='AppExplanation'
-          component={AppExplanation}
-        />
-
-        <Stack.Screen
           name='Home'
           component={Home}
         />
-
         <Stack.Screen
           name='HabitPage'
           component={HabitPage}
+        />
+        <Stack.Screen
+          name='AppExplanation'
+          component={AppExplanation}
         />
       </Stack.Navigator>
     </NavigationContainer>
